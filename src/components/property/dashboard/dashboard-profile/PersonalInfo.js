ea0c5bab960d5640f7a31a73eaabb803c0f7fb4e
Inu <thanhtaminu.com>
2025-05-13 22:08:39 +0700

    13/05/2025 22:08

--- src/components/property/dashboard/dashboard-profile/PersonalInfo.js
+++ src/components/property/dashboard/dashboard-profile/PersonalInfo.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { apiAuthen } from "@/apis/authen";
+import { useRouter } from "next/navigation";
 
 const PersonalInfo = () => {
   const router = useRouter();
@@ -16,6 +17,7 @@ const PersonalInfo = () => {
   const token = getCookie("token");
   if (!token) {
     // window.location.href = "/";
+    router.push("/");
   }
   const decodedToken = async () => {
     const token = getCookie("token");
